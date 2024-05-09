1587a3b429a44f33b7da1e33709082d1b864910a
Szykuc2001 <76168295+Szykuc2001@users.noreply.github.com>
2024-05-10 01:35:10 +0200

    Reworked Lab2 app
    Reworked lab2 app due to issues with Kotlin list

---
+++ Lab2/java/com/example/lab2/Recipe2Activity.java
@@ -0,0 +1,32 @@
+package com.example.lab2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Recipe2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recipe2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void openHome(View view) {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+    public void openShoppingList(View view) {
+        startActivity(new Intent(this, ShoppingListActivity.class));
+    }
+}
