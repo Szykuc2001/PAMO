992b89cdeb1f0952eac8e7ccfd002d9eccc26d67
Szykuc2001 <76168295+Szykuc2001@users.noreply.github.com>
2024-05-11 01:06:58 +0200

    Create Checkbox.kt

---
+++ Lab2/java/com/example/lab2/Checkbox.kt
@@ -0,0 +1,40 @@
+///Szymon Kuczyński s22466
+package com.example.lab2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Checkbox(private val itemList: List<CustomItem>) :
+        RecyclerView.Adapter<CheckboxAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_custom, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.textView.text = item.customText
+        holder.checkbox.isChecked = false // set checkbox state as per your requirement
+        holder.checkbox.setOnClickListener {
+            // Handle checkbox click event here
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val checkbox: CheckBox = itemView.findViewById(R.id.checkbox)
+        val textView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    // Define your custom item class
+    data class CustomItem(val customText: String)
+}
